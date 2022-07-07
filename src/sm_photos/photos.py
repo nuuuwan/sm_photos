e66d653dfb00883dd79458de952355a4c3451347
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-07 10:28:52 +0530

    Removed build_collage

--- src/sm_photos/photos.py
+++ src/sm_photos/photos.py
@@ -1,10 +1,6 @@
-import math
 import os
 
-from PIL import Image
-
-from sm_photos._constants import DIR_DATA, DIR_TWTR_DATA
-from sm_photos._utils import log
+from sm_photos._constants import DIR_TWTR_DATA
 
 WIDTH, HEIGHT = 80, 45
 
@@ -18,27 +14,3 @@ def get_photo_file_list():
         photo_file_list.append(os.path.join(DIR_TWTR_DATA, file_only))
     photo_file_list = list(reversed(list(sorted(photo_file_list))))
     return photo_file_list
-
-
-def build_collage():
-    photo_file_list = get_photo_file_list()
-    n = len(photo_file_list)
-    dim = math.ceil(math.sqrt(n))
-
-    image_collage = Image.new(mode="RGBA", size=(dim * WIDTH, dim * HEIGHT))
-    for i, photo_file in enumerate(photo_file_list):
-        image = Image.open(photo_file)
-        image = image.resize(size=(WIDTH, HEIGHT))
-
-        ix = i % dim
-        iy = (int)(i / dim)
-
-        image_collage.paste(image, (ix * WIDTH, iy * HEIGHT))
-    collage_file = os.path.join(DIR_DATA, 'collage.png')
-    image_collage.save(collage_file)
-
-    log.info(f'Wrote {n} images to {collage_file}')
-
-
-if __name__ == '__main__':
-    build_collage()
