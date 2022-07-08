4f50576d3fe5f8d28f0f2c5c44607651ebc9eedb
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-08 17:06:31 +0530

    Added expand_tweet_info_list

--- src/sm_photos/tweet_info_utils.py
+++ src/sm_photos/tweet_info_utils.py
@@ -6,6 +6,56 @@ from sm_photos._constants import DIR_TWTR_DATA
 from sm_photos._utils import log
 
 
+def organize_media(file_only_list):
+    media = {'photo': [], 'video': [], 'video_clip': []}
+    for file_only in file_only_list:
+        if file_only[-4:] == '.mp4':
+            media_type = 'video'
+        if file_only[-4:] in ['.png', '.jpg']:
+            if '.clip.' in file_only:
+                media_type = 'video_clip'
+            else:
+                media_type = 'photo'
+
+        media[media_type].append(file_only)
+    return file_only
+
+
+def get_id_to_media(tweet_info_list):
+    id_to_file_only_list = {}
+    for file_only in os.listdir(DIR_TWTR_DATA):
+        id = file_only.partition('.')[0]
+        if id not in id_to_file_only_list:
+            id_to_file_only_list[id] = []
+
+        if file_only[-4:] in ['.png', '.jpg', '.mp4']:
+            id_to_file_only_list[id].append(file_only)
+
+    return dict(
+        list(
+            map(
+                lambda item: [item[0], organize_media(item[1])],
+                id_to_file_only_list.items(),
+            )
+        )
+    )
+
+
+def expand_tweet_info(tweet_info, id_to_media):
+    tweet_info['local_media'] = id_to_media[tweet_info['id']]
+    return tweet_info
+
+
+def expand_tweet_info_list(tweet_info_list):
+    id_to_media = get_id_to_media(tweet_info_list)
+    return list(
+        map(
+            lambda tweet_info: expand_tweet_info(tweet_info, id_to_media),
+            tweet_info_list,
+        )
+    )
+
+
 def load_tweet_info_list():
     tweet_info_list = []
     for file_name_only in os.listdir(DIR_TWTR_DATA):
@@ -21,4 +71,4 @@ def load_tweet_info_list():
         tweet_info_list,
         key=lambda tweet_info: -tweet_info['time_create_ut'],
     )
-    return tweet_info_list
+    return expand_tweet_info_list(tweet_info_list)
