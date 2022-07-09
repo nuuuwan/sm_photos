e5fa98c1aee9d044b7e08f0c7acf090739746aa2
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-09 12:24:57 +0530

    Run all

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -8,21 +8,20 @@ from sm_photos.twtr import TWTR
 def main(hashtag):
     filesys.init()
 
-    if False:
-        twtr = TWTR()
-        tweet_info_list = twtr.get_tweet_info_list(hashtag)
-        for tweet_info in tweet_info_list:
-            filesys.download_and_save(tweet_info)
+    twtr = TWTR()
+    tweet_info_list = twtr.get_tweet_info_list(hashtag)
+    for tweet_info in tweet_info_list:
+        filesys.download_and_save(tweet_info)
 
-        videos.backpopulate_video_clips()
-        summary.build_summary()
+    videos.backpopulate_video_clips()
+    summary.build_summary()
 
-        collage.build_collage()
-        base_image_file = 'media/text.sketch.png'
-        photo_file_list = dedupe.dedupe_photos(photos.get_photo_file_list())
-        text_collage.build_text_collage(base_image_file, photo_file_list)
-        text_collage.metarize()
-        readme.build_readme()
+    collage.build_collage()
+    base_image_file = 'media/text.sketch.png'
+    photo_file_list = dedupe.dedupe_photos(photos.get_photo_file_list())
+    text_collage.build_text_collage(base_image_file, photo_file_list)
+    text_collage.metarize()
+    readme.build_readme()
 
     photo_video.build_photo_video_all()
 
