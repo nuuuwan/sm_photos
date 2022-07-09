57b46792187f23f287ecc0ce42aaa5e54c3b72c3
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-09 12:02:52 +0530

    Added URL_REMOTE_TWTR_DATA

--- src/sm_photos/_constants.py
+++ src/sm_photos/_constants.py
@@ -5,6 +5,11 @@ URL_GIT_REPO = 'https://github.com/nuuuwan/sm_photos'
 DIR_DATA = '/tmp/sm_photos.data'
 DIR_TWTR_DATA = os.path.join(DIR_DATA, 'twtr_data')
 
+URL_REMOTE_TWTR_DATA = os.path.join(
+    'https://raw.githubusercontent.com',
+    'nuuuwan/sm_photos',
+    'data/twtr_data',
+)
 
 USER_BLACKLIST = [
     'udeshan',
