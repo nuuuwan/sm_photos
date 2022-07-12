fd1ac7043dbed203c8ce9e0047f2ba6fe757b49c
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-12 16:57:45 +0530

    Updated pipeline

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,11 +1,6 @@
 import argparse
-# import os
-# import shutil
 
-from sm_photos import (collage, dedupe, filesys, photo_video, photos, readme,
-                       summary, text_collage, videos)
-from sm_photos._constants import DIR_TWTR_DATA
-from sm_photos._utils import log
+from sm_photos import filesys, readme, summary
 from sm_photos.twtr import TWTR
 
 
@@ -17,20 +12,8 @@ def main(hashtag):
     for tweet_info in tweet_info_list:
         filesys.download_and_save(tweet_info)
 
-    videos.backpopulate_video_clips()
     summary.build_summary()
-
-    collage.build_collage()
-    base_image_file = 'media/text.sketch.png'
-    photo_file_list = dedupe.dedupe_photos(photos.get_photo_file_list())
-    text_collage.build_text_collage(base_image_file, photo_file_list)
-    text_collage.metarize()
     readme.build_readme()
-
-    try:
-        photo_video.build_photo_video_all()
-    except Exception as e:
-        log.error(str(e))
 
 
 def get_options():
